610f69cfa71334ce096133d1e3ce1f258582ec82
JayArindam <jayarindammaity08@gmail.com>
2025-08-18 19:04:12 +0530

    added more features and tests

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,26 @@
 import "dotenv/config";
+import express from "express";
+import authRoutes from "./routes/auth.routes";
+import testRoutes from "./routes/test.routes";
+import rateLimit from "express-rate-limit";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+app.use(limiter);
+
+// Routes
+app.use("/api", testRoutes);
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
